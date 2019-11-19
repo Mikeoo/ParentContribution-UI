fec33ddd5a835bd07829a9f6abebe41f5fe2c627
Mike Overbeek <mikeoverbeek@outlook.com>
2019-11-19 15:07:28 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ParentContribution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.numberOfKidsAdded = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 443);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add birthdate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(46, 59);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            // 
+            // numberOfKidsAdded
+            // 
+            this.numberOfKidsAdded.Location = new System.Drawing.Point(263, 584);
+            this.numberOfKidsAdded.Name = "numberOfKidsAdded";
+            this.numberOfKidsAdded.Size = new System.Drawing.Size(68, 31);
+            this.numberOfKidsAdded.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 587);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of kids added.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(86, 635);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(86, 388);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(183, 29);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Single parent?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(69, 504);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(231, 55);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Remove last birthdate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(547, 316);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1286, 852);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numberOfKidsAdded);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox numberOfKidsAdded;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParentContribution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> childs = new List<string>();
+        int addedKids = 0;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            childs.Add(monthCalendar1.SelectionRange.ToString());
+            addedKids += 1;
+            numberOfKidsAdded.Text = Convert.ToString(addedKids);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            childs.RemoveAt(childs.Count - 1);
+            addedKids -= 1;
+            numberOfKidsAdded.Text = Convert.ToString(addedKids);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DateTime today = DateTime.Now;
+            DateTime kid = Convert.ToDateTime(childs[1]);
+            int age = today.Year - kid.Year;
+            if (today.Month < kid.Month)
+            {
+                age--;
+            }
+            else if (today.Month == kid.Month && today.Day < kid.Day)
+            {
+                age--;
+            }
+            textBox1.Text = Convert.ToString(age);
+        }
+    }
+}
